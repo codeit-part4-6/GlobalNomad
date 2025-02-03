ebdf9228984962524d163dad4154f783022ac10a
whdjh <whdjh98@naver.com>
2025-02-03 19:16:13 +0900

    [feat]: provider -> kakao로 수정

--- service/api/oauth/postOAuth.api.tsx
+++ service/api/oauth/postOAuth.api.tsx
@@ -1,13 +1,16 @@
 import INSTANCE_URL from '../instance';
 
-export async function postOAuth(provider: string) {
-  const appKey = process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID;
-  const data = { 
-    appKey, 
-    provider, 
-  };
+export async function postOAuth() {  
+  const appKey = process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID;  
+  const provider = 'kakao';
 
-  const response = await INSTANCE_URL.post('/oauth/apps', data);
-  console.log('API 응답 데이터:', response.data);
+  const data = {     
+    appKey,     
+    provider,   
+  };  
+
+  const response = await INSTANCE_URL.post('/oauth/apps', data);  
+  console.log('API 응답 데이터:', response.data);  
+
   return response;
 }
