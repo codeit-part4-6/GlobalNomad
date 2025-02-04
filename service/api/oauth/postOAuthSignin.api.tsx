c31e4578b39ab70d6794097306e5f2c82bcd22cf
whdjh <whdjh98@naver.com>
2025-02-04 16:02:37 +0900

    [feat]: api 수정

--- service/api/oauth/postOAuthSignin.api.tsx
+++ service/api/oauth/postOAuthSignin.api.tsx
@@ -3,5 +3,5 @@ import INSTANCE_URL from '../instance';
 export async function postOAuthSignin(body: { redirectUri: string; token: string }) {
   const response = await INSTANCE_URL.post('/oauth/sign-in/kakao', body);
   console.log('API 응답 데이터:', response.data);
-  return response;
+  return response.data;
 }
