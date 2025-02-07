265fd776a43d83fa152221fdeb69833a39e528ee
whdjh <whdjh98@naver.com>
2025-02-07 18:07:36 +0900

    [feat]: token은 인가코드로 변경

--- app/oauth/kakao/page.tsx
+++ app/oauth/kakao/page.tsx
@@ -1,6 +1,5 @@
 'use client';
-
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { useMutation } from '@tanstack/react-query';
 import { postOAuth } from '@/service/api/oauth/postOAuth.api';
@@ -8,27 +7,32 @@ import { postOAuth } from '@/service/api/oauth/postOAuth.api';
 export default function KakaoOAuthCallbackPage() {
   const router = useRouter();
   const searchParams = useSearchParams();
-  const hasCodeBeenProcessed = useRef(false);
   
   const oauthMutation = useMutation({
     mutationFn: postOAuth,
     onSuccess: (data) => {
-      console.log(data)
-      sessionStorage.setItem('accessToken', data.accessToken);
-      sessionStorage.setItem('refreshToken', data.refreshToken);
-      sessionStorage.setItem('userInfo', JSON.stringify(data.user));
-      router.push('/');
+      console.log(data);
+      const code = searchParams.get('code');
+      if (code) {
+        // 세션 스토리지에 토큰 및 사용자 정보 저장
+        sessionStorage.setItem('token', code);
+        sessionStorage.setItem('userInfo', JSON.stringify(data));
+        // 홈페이지로 리다이렉트
+        router.push('/');
+      } else {
+        console.error('Code not found in URL');
+        router.push('/signin');
+      }
     },
     onError: (error) => {
       console.error('OAuth 로그인 실패', error);
       router.push('/signin');
-    }
+    },
   });
 
   useEffect(() => {
     const code = searchParams.get('code');
-    if (code && !hasCodeBeenProcessed.current) {
-      hasCodeBeenProcessed.current = true;
+    if (code) {
       oauthMutation.mutate(code);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
