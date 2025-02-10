2827b6470b69dec6ce9d87765b55b388c9c05379
yunrap <dbsdpwl7@gmail.com>
2025-02-10 13:44:03 +0900

    [feat] 체험수정 버튼디자인변경

--- components/myactivities/myactivities.tsx
+++ components/myactivities/myactivities.tsx
@@ -146,38 +146,40 @@ export default function MyActivities({onclose}: {onclose: () => void}) {
   return (
     <>
       <div className="flex flex-col">
-        <div className="mb-2 flex justify-end tablet:hidden">
-          <Image onClick={onclose} src={closeButton} alt="모달 닫기 버튼" className="cursor-pointer" width={48} height={48} />
-        </div>
         <div className="mb-16 h-full w-full">
           <div className="item-center mb-4 flex justify-between tablet:mb-6 tablet:mt-0">
             <h1 className="text-3xl font-bold">{content === 'manage' ? '내 체험 관리' : '내 체험 등록'}</h1>
-            {content === 'manage' ? (
-              <>
+            <div className="flex gap-1">
+              {content === 'manage' ? (
+                <>
+                  <Button
+                    onClick={() => setContent('register')}
+                    className="h-48pxr w-120pxr gap-4pxr rounded-md bg-primary pb-8pxr pl-16pxr pr-16pxr pt-8pxr text-white"
+                  >
+                    등록하기
+                  </Button>
+                </>
+              ) : content === 'register' ? (
                 <Button
-                  onClick={() => setContent('register')}
-                  className="h-[48px] w-[120px] gap-[4px] rounded-[4px] bg-primary pb-[8px] pl-[16px] pr-[16px] pt-[8px] text-white"
+                  onClick={triggerSubmit} // 버튼 클릭 시 자식 컴포넌트의 폼 제출 트리거
+                  className={`h-48pxr w-120pxr gap-4pxr rounded-md pb-8pxr pl-16pxr pr-16pxr pt-8pxr text-white ${
+                    isValid ? 'bg-primary' : 'bg-gray-500'
+                  }`}
                 >
-                  체험 등록하기
+                  등록하기
                 </Button>
-              </>
-            ) : content === 'register' ? (
-              <Button
-                onClick={triggerSubmit} // 버튼 클릭 시 자식 컴포넌트의 폼 제출 트리거
-                className={`h-[48px] w-[120px] gap-[4px] rounded-[4px] pb-[8px] pl-[16px] pr-[16px] pt-[8px] text-white ${
-                  isValid ? 'bg-primary' : 'bg-gray-500'
-                }`}
-              >
-                등록하기
-              </Button>
-            ) : (
-              <Button
-                onClick={triggerSubmit}
-                className={`${isValidModify ? 'bg-primary' : 'bg-gray-500'} h-[48px] w-[120px] gap-[4px] rounded-[4px] pb-[8px] pl-[16px] pr-[16px] pt-[8px] text-white`}
-              >
-                수정하기
-              </Button>
-            )}
+              ) : (
+                <Button
+                  onClick={triggerSubmit}
+                  className={`${isValidModify ? 'bg-primary' : 'bg-gray-500'} h-48pxr w-120pxr gap-4pxr rounded-md pb-8pxr pl-16pxr pr-16pxr pt-8pxr text-white`}
+                >
+                  수정하기
+                </Button>
+              )}
+              <div className="mb-2 flex justify-end tablet:hidden">
+                <Image onClick={onclose} src={closeButton} alt="모달 닫기 버튼" className="cursor-pointer" width={48} height={48} />
+              </div>
+            </div>
           </div>
 
           {content === 'manage' && (
