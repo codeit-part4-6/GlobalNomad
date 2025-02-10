5ab9c944753b8cdca87dcb7f6af0d6235c44732a
twtwkim <alfog23@gmail.com>
2025-02-10 23:34:44 +0900

    [style]: 마이페이지 ui 적용

--- app/mypage/layout.tsx
+++ app/mypage/layout.tsx
@@ -8,13 +8,28 @@ export default function MyPageLayout({side, menu}: {side: React.ReactNode; menu:
   const isModalOpen = searchParams.get('modal') === 'true';
   return (
     <>
+      {/* pc, 태블릿 */}
       <div className="hidden min-h-740pxr bg-black-400 tablet:block">
-        <div>{side}</div>
-        <div>{menu}</div>
+        <div className="mx-auto px-4 pt-6 tablet:p-6 pc:w-full pc:max-w-[75rem] pc:p-0 pc:pt-[4.5rem]">
+          <div className="flex tablet:gap-4 pc:gap-6">
+            <aside>{side}</aside>
+            <div className="flex-grow">{menu}</div>
+          </div>
+        </div>
       </div>
+
+      {/* 모바일 */}
       <div className="min-h-740pxr bg-black-400 tablet:hidden">
-        <div>{side}</div>
-        {isModalOpen && <OverlayContainer>{menu}</OverlayContainer>}
+        <div className="mx-auto px-4 pt-6 tablet:p-6 pc:w-full pc:max-w-[75rem] pc:p-0 pc:pt-[4.5rem]">
+          <aside>{side}</aside>
+          {isModalOpen && (
+            <OverlayContainer>
+              <div className="h-full w-full overflow-y-auto bg-white">
+                <div className="min-h-740pxr bg-black-400 px-4 pt-6">{menu}</div>
+              </div>
+            </OverlayContainer>
+          )}
+        </div>
       </div>
     </>
   );
