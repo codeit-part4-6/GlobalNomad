c081e8f32bbad2117569dfb751cf625fdbc4c60c
yunrap <dbsdpwl7@gmail.com>
2025-02-18 16:56:33 +0900

    [refactor] 체험관리 로딩스피너 추가

--- components/side-navigation/mypage.tsx
+++ components/side-navigation/mypage.tsx
@@ -1,17 +1,17 @@
 'use client';
 
-import { useEffect, useState } from 'react';
-import { useForm, Controller } from 'react-hook-form';
-import { useMutation } from '@tanstack/react-query';
-import { useRouter } from 'next/navigation';
+import {useEffect, useState} from 'react';
+import {useForm, Controller} from 'react-hook-form';
+import {useMutation} from '@tanstack/react-query';
+import {useRouter} from 'next/navigation';
 import Image from 'next/image';
-import { EditMypageBody } from '@/types/patchMypage.types';
+import {EditMypageBody} from '@/types/patchMypage.types';
 import Input from '@/components/common/Input';
 import Button from '@/components/common/button';
 import Modal from '@/components/common/modal/modal';
-import { patchMypage } from '@/service/api/users/patchMypage.api';
-import { useAuthStore } from '@/service/store/authStore';
-import { useImageUrlStore } from '@/service/store/imageURLStore';
+import {patchMypage} from '@/service/api/users/patchMypage.api';
+import {useAuthStore} from '@/service/store/authStore';
+import {useImageUrlStore} from '@/service/store/imageURLStore';
 import closeButton from '@/public/icon/ic_close_button.svg';
 
 interface IFormInput {
