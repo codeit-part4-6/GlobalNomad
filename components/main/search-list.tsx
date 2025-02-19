ab502c1eb6b08513557f26426e8235ffab937940
twtwkim <alfog23@gmail.com>
2025-02-19 11:24:26 +0900

    [debug]: search-list 페이지네이션 오류 수정 반영

--- components/main/search-list.tsx
+++ components/main/search-list.tsx
@@ -91,7 +91,7 @@ export default function SearchList({keyword}: SearchListProps): JSX.Element {
           <ScaleLoader />
         </div>
       )}
-      <Pagenation page={page} size={searchActivities?.totalCount} showItemCount={pageSize} onChange={handlePageChange} />
+      {searchActivities && <Pagenation page={page} size={searchActivities?.totalCount} showItemCount={pageSize} onChange={handlePageChange} />}
     </div>
   );
 }
